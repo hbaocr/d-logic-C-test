f130dd0ee8af4eb6c78cc0ac66bc86a817fc19ce
Duong Huynh Bao <duonghuynhbaocr@gmail.com>
2019-11-19 08:50:51 +0700

    add check lib version

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 using namespace std;
 int main(int argc, const char * argv[]) {
+    const char *lib_ver = GetDllVersionStr();
+    std::cout << "Share Lib Version :  " << lib_ver<<endl;
     uint32_t reader_type =1; // 1 : uFR type (1 Mbps)
     const char *port_name ="/dev/tty.usbserial-A631LPCL";
     uint32_t port_interface=1; // 1 : try serial / virtual COM port / interfaces
     const char *arg ="READER_ACTIVE_ON_RTS_LOW"; //arg : additional settings in c-string format
 
     int res = ReaderOpenEx(reader_type,port_name,port_interface,(void *) arg);
-    std::cout << "Status Code :  " << res;
+    std::cout << "Status Code :  " << res<<endl;
     return 0;
 }
